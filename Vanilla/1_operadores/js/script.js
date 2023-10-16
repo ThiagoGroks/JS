e36e5ac257ff4ef1808853ac1ea7124ed56bb036
ThiagoGroks <thiago.groks@gmail.com>
2023-10-16 16:51:18 -0300

    Tipos de dados e opradores em JS

---
+++ Vanilla/1_operadores/js/script.js
@@ -0,0 +1,31 @@
+//Number
+console.log(typeof 2);
+console.log(typeof -356);
+console.log(typeof 8.357);
+
+//Ops aritméticas
+console.log(2+4);
+console.log(333-12);
+
+//Special numbers
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof NaN);
+
+//String
+console.log("Cacareco")
+console.log("135")
+console.log("Um texto em \nduas linhas")
+console.log("Espaço de \ttab")
+
+//Template strings
+//interpolação
+console.log(`A soma de 2 + 2 é: ${2 + 2}`)
+
+//Booleans
+console.log(3 > 2)
+console.log(55 === 12)
+
+//empty values
+console.log(typeof null, typeof undefined)
+
